88a92273150f15337336a175f50ad10acd8b5b0f
hoangngocthang <>
2023-02-11 20:41:31 +0700

    RN 0.66.5

--- src/ui/TimePickerCheckout.tsx
+++ src/ui/TimePickerCheckout.tsx
@@ -2,9 +2,9 @@ import {TextInput, View, StyleSheet, Text, TouchableOpacity} from "react-native"
 import React, {Component} from "react";
 import MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunityIcons';
 import DateTimePicker from 'react-native-modal-datetime-picker';
-import { Appearance } from 'react-native-appearance';
+// import { Appearance } from 'react-native-appearance';
 import moment from "moment";
-const colorScheme = Appearance.getColorScheme();
+// const colorScheme = Appearance.getColorScheme();
 export default class TimePickerCheckout extends Component<any>{
 
     constructor(props: any) {
